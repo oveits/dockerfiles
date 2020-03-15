609230b629a5155a8c8517fd02f5632101d7e8b0
Oliver Veits <oliver.veits@gmail.com>
2020-03-15 23:12:58 +0100

    cleaning

--- maven-as-current-user/1_entrypoint-user.sh
+++ maven-as-current-user/1_entrypoint-user.sh
@@ -2,7 +2,8 @@
 
 if [ "$(id -u)" == "0" ]; then
   groupadd $MYGROUP -g $MYGID \
-    && useradd -m $MYUSER -u $MYUID -g $MYGID -d /home/user 
+    && useradd -m $MYUSER -u $MYUID -g $MYGID -d /home/user  \
+    &&  while true; do echo waiting for process to be killed...; sleep 60; done
+else
+  exec $@
 fi
-
-exec $@
--- maven-as-current-user/2_entrypoint_init_and_run_mvn.sh
+++ maven-as-current-user/2_entrypoint_init_and_run_mvn.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-if [ "$(id -u)" == "0" ]; then
-    while true; do echo waiting for process to be killed...; sleep 60; done
-else
-  /usr/local/bin/mvn-entrypoint.sh \
-    && exec $@
-fi
+/usr/local/bin/mvn-entrypoint.sh $@
 
